1b8c7721d431baaa004fabfe1acd82ecd209b6e5
sreddy11 <sreddy1.sr@gmail.com>
2013-06-13 19:13:31 -0400

    Took Mike's feedback

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Hackernews::Application.routes.draw do
   resources :users
   resources :articles
   resource :login 
+ 
+  match "login" => "logins#new"
   
   controller :logins do
-    get 'login' => :new
-    post 'login' => :create
     delete 'logout' => :destroy
   end
   # The priority is based upon order of creation:
