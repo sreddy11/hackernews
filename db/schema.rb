f684ed1ef855bd8349deefadda96c8d3d900343b
sreddy11 <sreddy1.sr@gmail.com>
2013-06-17 11:05:09 -0400

    Removed page column from database

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(:version => 20130612211219) do
     t.string   "title",      :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "page"
     t.integer  "user_id"
   end
 
