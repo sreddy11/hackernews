815dd86394c42195eff30b972b6209b6d2687199
sreddy11 <sreddy1.sr@gmail.com>
2013-06-19 14:47:44 -0400

    Made minor changes

--- db/migrate/20130615024040_create_comments.rb
+++ db/migrate/20130615024040_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
 
-      t.text :body, :null => false 
+      t.text :body 
       t.timestamps
     end
   end
