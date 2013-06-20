e96d280279475a585decf26d5eda1501c494c931
sreddy11 <sreddy1.sr@gmail.com>
2013-06-20 09:19:15 -0400

    Added spec to article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -22,5 +22,6 @@ describe Article do
   it { should validate_presence_of(:url) }
   it { should allow_value('https://cnn.com').for(:url) }
   it { should_not allow_value('not_a_url').for(:url) }
+  it { should have_many(:comments) }
 
 end
