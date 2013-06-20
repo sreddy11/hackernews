edd1b8e70c104a8c6a60e5ec7f235b0e74c5b99a
sreddy11 <sreddy1.sr@gmail.com>
2013-06-20 15:02:56 -0400

    Made comments more acessible in view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,8 @@ class ArticlesController < ApplicationController
   end
  
   def show
-   @article = Article.find(params[:id]) 
+    @comment = Comment.new
+    @article = Article.find(params[:id]) 
   end
 
   def new
