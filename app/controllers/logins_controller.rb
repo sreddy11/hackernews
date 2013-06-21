c53147b2e04b47800cf4be5ec4ce9562d721674c
sreddy11 <sreddy1.sr@gmail.com>
2013-06-21 14:33:17 -0400

    Made sure user was logged in before they could like things

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -10,7 +10,11 @@ class LoginsController < ApplicationController
     if @user && @user.authenticate(login_params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "Login Successful"
-      redirect_to(articles_path)
+      begin
+        redirect_to(:back)
+      rescue ActionController::RedirectBackError
+        redirect_to(articles_path)
+      end
       session[:return_page] = nil
     else
       flash.now[:error] = "Login Invalid"
