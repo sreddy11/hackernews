45df4df0eaab1208f33bda5f5a33c7cb9da5f415
sreddy11 <sreddy1.sr@gmail.com>
2013-06-23 13:29:35 -0400

    Added spec tests

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,34 +1,31 @@
 class RatingsController < ApplicationController
 
-def index
-  
-  @filter = params[:filter]
-  unless params[:filter_date].nil?
-  @year = params[:filter_date][:year].to_i
-  @month = params[:filter_date][:month].to_i
-  @day = params[:filter_date][:day].to_i
-  @date = DateTime.new(@year, @month, @day)
-  
-    if @filter == "Year"
-      @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
-                           {:start => @date.beginning_of_year, :end => @date.end_of_year }]).
-                           sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+  def index
+    
+    @filter = params[:filter]
+    unless params[:filter_date].nil?
+      @year = params[:filter_date][:year].to_i
+      @month = params[:filter_date][:month].to_i
+      @day = params[:filter_date][:day].to_i
+      @date = DateTime.new(@year, @month, @day)
+    
+      if @filter == "Year"
+        @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
+                             {:start => @date.beginning_of_year, :end => @date.end_of_year }]).
+                             sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+      end
+      if @filter == "Month"
+        @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
+                             {:start => @date.beginning_of_month, :end => @date.end_of_month }]).
+                             sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+      end
+      if @filter == "Day"
+        @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
+                             {:start => @date.beginning_of_day, :end => @date.end_of_day }]).
+                             sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+      end
     end
-    if @filter == "Month"
-      @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
-                           {:start => @date.beginning_of_month, :end => @date.end_of_month }]).
-                           sort {|b,a| a.num_upvotes <=> b.num_upvotes}
-    end
-    if @filter == "Day"
-      @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
-                           {:start => @date.beginning_of_day, :end => @date.end_of_day }]).
-                           sort {|b,a| a.num_upvotes <=> b.num_upvotes}
-    end
-
-
-
   end
-end
 
 
 end
--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -2,4 +2,53 @@ require 'spec_helper'
 
 describe RatingsController do
 
+  describe "GET to #index" do
+  
+    context "filtered by year" do
+      let!(:article) {FactoryGirl.create(:article, {:created_at => DateTime.new(2012,5,5)})} 
+      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5)})}
+
+      before do
+        get :index, {:filter => "Year", :filter_date => {:year => 2012, :month => 5, :day => 5}}
+      end
+
+      it "should retrieve one article" do
+        assigns[:ratings].should == [article]
+      end
+    
+    end
+
+    context "filtered by month" do
+
+      let!(:article) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,5)})} 
+      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5)})}
+
+      before do
+        get :index, {:filter => "Month", :filter_date => {:year => 2013, :month => 6, :day => 5}}
+      end
+
+      it "should retrieve one article" do
+        assigns[:ratings].should == [article]
+      end
+
+    
+    end
+
+    context "filtered by day" do
+
+      let!(:article) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,5)})} 
+      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,6)})}
+
+      before do
+        get :index, {:filter => "Day", :filter_date => {:year => 2013, :month => 6, :day => 5}}
+      end
+
+      it "should retrieve one article" do
+        assigns[:ratings].should == [article]
+      end
+    
+    end
+  
+  end
+
 end
