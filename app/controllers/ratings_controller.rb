9aa3e5e019245fc13c7a0e87797809f7bec3d0ea
sreddy11 <sreddy1.sr@gmail.com>
2013-06-22 23:05:04 -0400

    Created Search filters

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -8,8 +8,25 @@ def index
   @month = params[:filter_date][:month].to_i
   @day = params[:filter_date][:day].to_i
   @date = DateTime.new(@year, @month, @day)
-  @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
-                         {:start => @date.beginning_of_year, :end => @date.end_of_year }])
+  
+    if @filter == "Year"
+      @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
+                           {:start => @date.beginning_of_year, :end => @date.end_of_year }]).
+                           sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+    end
+    if @filter == "Month"
+      @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
+                           {:start => @date.beginning_of_month, :end => @date.end_of_month }]).
+                           sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+    end
+    if @filter == "Day"
+      @ratings = Article.all(:conditions => ["created_at >= :start AND created_at <= :end", 
+                           {:start => @date.beginning_of_day, :end => @date.end_of_day }]).
+                           sort {|b,a| a.num_upvotes <=> b.num_upvotes}
+    end
+
+
+
   end
 end
 
