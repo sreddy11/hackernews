b3270f4bd8030775ccf84d9e0dbc927fa81d08e7
sreddy11 <sreddy1.sr@gmail.com>
2013-06-25 16:18:52 -0400

    Added spec tests for ratings sort

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -6,14 +6,15 @@ describe RatingsController do
   
     context "filtered by year" do
       let!(:article) {FactoryGirl.create(:article, {:created_at => DateTime.new(2012,5,5)})} 
-      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5)})}
+      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5), :rating => 3})}
+      let!(:article3) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5), :rating => 2})}
 
       before do
-        get :index, {:filter => "Year", :filter_date => {:year => 2012, :month => 5, :day => 5}}
+        get :index, {:filter => "Year", :filter_date => {:year => 2013, :month => 5, :day => 5}}
       end
 
-      it "should retrieve one article" do
-        assigns[:ratings].should == [article]
+      it "should retrieve two ordered articles" do
+        assigns[:ratings].should == [article2,article3]
       end
     
     end
@@ -21,14 +22,14 @@ describe RatingsController do
     context "filtered by month" do
 
       let!(:article) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,5)})} 
-      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5)})}
-
+      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5), :rating => 3})}
+      let!(:article3) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,5,5), :rating => 2})}
       before do
-        get :index, {:filter => "Month", :filter_date => {:year => 2013, :month => 6, :day => 5}}
+        get :index, {:filter => "Month", :filter_date => {:year => 2013, :month => 5, :day => 5}}
       end
 
-      it "should retrieve one article" do
-        assigns[:ratings].should == [article]
+      it "should retrieve two ordered articles" do
+        assigns[:ratings].should == [article2, article3]
       end
 
     
@@ -37,14 +38,14 @@ describe RatingsController do
     context "filtered by day" do
 
       let!(:article) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,5)})} 
-      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,6)})}
-
+      let!(:article2) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,6), :rating => 3})}
+      let!(:article3) {FactoryGirl.create(:article, {:created_at => DateTime.new(2013,6,6), :rating => 2})}
       before do
-        get :index, {:filter => "Day", :filter_date => {:year => 2013, :month => 6, :day => 5}}
+        get :index, {:filter => "Day", :filter_date => {:year => 2013, :month => 6, :day => 6}}
       end
 
       it "should retrieve one article" do
-        assigns[:ratings].should == [article]
+        assigns[:ratings].should == [article2, article3]
       end
     
     end
